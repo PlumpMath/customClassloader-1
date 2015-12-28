695178a89f2fc88218959c169699c9cb9da83091
aldos2002 <aldos2002@gmail.com>
2015-12-28 12:04:30 +0600

    Project structure change

---
+++ Variant1/src/com/module/PrinterVariant1.java
@@ -0,0 +1,10 @@
+package com.module;
+
+/**
+ * Created by admin on 28.12.2015.
+ */
+public class PrinterVariant1 {
+    public String print(){
+        return "Variant1";
+    }
+}
---
+++ Variant2/src/com/module/PrinterVariant2.java
@@ -0,0 +1,10 @@
+package com.module;
+
+/**
+ * Created by admin on 28.12.2015.
+ */
+public class PrinterVariant2 {
+    public String print(){
+        return "Variant2";
+    }
+}
